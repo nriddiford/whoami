7e87a2c920d141d319075fe09fe24f34266f9528
nriddiford <nickriddiford@me.com>
2018-05-21 16:31:14 +0200

    Minor improvements

--- imapy.py
+++ imapy.py
@@ -13,13 +13,18 @@ import email
 import email.header
 import datetime
 
+import time
+start_time = time.time()
+
+from optparse import OptionParser
+
+
 from email_reply_parser import EmailReplyParser
 
-EMAIL_ACCOUNT = "nickriddiford@gmail.com"
 EMAIL_FOLDER = "[Gmail]/Sent Mail"
 
 
-def process_mailbox(M):
+def process_mailbox(M, options):
     """
     Do something with emails messages in the folder.
     For the sake of this example, print some headers.
@@ -30,12 +35,18 @@ def process_mailbox(M):
         print "No messages found!"
         return
 
-    with open('emails2.tsv', 'w') as outFile:
+    with open(options.output_file, 'w') as outFile:
+        message_count = 0
         for num in data[0].split():
             rv, data = M.fetch(num, '(RFC822)')
             if rv != 'OK':
                 print "ERROR getting message", num
                 return
+            if message_count % 100 == 0 and message_count > 0:
+                runTime = round((time.time() - start_time),1)
+                print("Read %d messages in %s seconds") % (message_count, runTime)
+
+            message_count += 1
 
             msg = email.message_from_string(data[0][1])
             decode = email.header.decode_header(msg['Subject'])[0]
@@ -55,13 +66,7 @@ def process_mailbox(M):
             else:
                 body = msg.get_payload(decode=True)
 
-
-            # print 'Message %s: %s' % (num, subject)
-            # print 'Raw Date:', msg['Date']
-            # print 'Adressee:', msg['to']
-            # print body
-
-            # Now convert to local date-time
+           # Now convert to local date-time
             date_tuple = email.utils.parsedate_tz(msg['Date'])
             if date_tuple:
                 local_date = datetime.datetime.fromtimestamp(
@@ -76,27 +81,58 @@ def process_mailbox(M):
             outFile.write("%s\t%s\t%s\t%s\n" % (msg['from'], msg['to'], send_date, reply_string))
 
 
-M = imaplib.IMAP4_SSL('imap.gmail.com')
+def get_args():
+    parser = OptionParser()
 
-try:
-    rv, data = M.login(EMAIL_ACCOUNT, getpass.getpass())
-except imaplib.IMAP4.error:
-    print "LOGIN FAILED!!! "
-    sys.exit(1)
+    parser.add_option("-e",
+                      "--email_address",
+                      dest="email_address",
+                      action="store",
+                      help="Your Gmail email address")
 
-print rv, data
+    parser.add_option("-o",
+                      "--output_file",
+                      dest="output_file",
+                      action="store",
+                      help="Name of output file [Default: 'emails.tsv'")
 
-rv, mailboxes = M.list()
-if rv == 'OK':
-    print "Mailboxes:"
-    print mailboxes
+    parser.set_defaults(output_file='emails.tsv')
 
-rv, data = M.select(EMAIL_FOLDER)
-if rv == 'OK':
-    print "Processing mailbox...\n"
-    process_mailbox(M)
-    M.close()
-else:
-    print "ERROR: Unable to open mailbox ", rv
+    options, args = parser.parse_args()
+    return (options, args)
 
-M.logout()
+
+
+def main():
+    options, args = get_args()
+    print options
+
+    M = imaplib.IMAP4_SSL('imap.gmail.com')
+
+    if options.email_address:
+        try:
+            rv, data = M.login(options.email_address, getpass.getpass())
+        except imaplib.IMAP4.error:
+            print "LOGIN FAILED!!! "
+            sys.exit(1)
+
+        print rv, data
+
+        rv, mailboxes = M.list()
+        # if rv == 'OK':
+        #     print "Mailboxes:"
+        #     print mailboxes
+
+        rv, data = M.select(EMAIL_FOLDER)
+        if rv == 'OK':
+            print "Processing mailbox...\n"
+            process_mailbox(M, options)
+            M.close()
+        else:
+            print "ERROR: Unable to open mailbox ", rv
+
+        M.logout()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
